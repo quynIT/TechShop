3903e03cb6e5af72947bc5b95da093584c3818f0
quynIT <trungquyen2902@gmail.com>
2024-09-22 03:35:39 +0700

    interface-page

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,11 @@
+
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Homepage from "../components/HomePage/Homepage";
+import TrendSlide from "../components/TrendSlide/TrendSlide";
+export const routes = [
+    {
+        path: '/',
+        page: Homepage
+    }
+]

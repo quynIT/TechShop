017dbfb01da5e0cea016f2f8e64cf18eecfbd5b7
nguyenngoctieuthu <Nguyenngoctieuthu2207@gmail.com>
2024-09-22 05:00:03 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './pages/auth/Sign_in';
-import SignUp from './pages/auth/Sign_up';
-import Dashboard from './pages/admin/Dashboard';
+
+
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     <button className='bg-blue-500 p-4 '>
-    //       Submit
-    //     </button>
-    //   </header>
-    // </div>
-    <SignUp/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button className='bg-blue-500 p-4 '>
+          Submit
+        </button>
+      </header>
+    </div>
   );
 }
 

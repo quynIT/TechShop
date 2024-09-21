5905d32abb8518216139c8c8312d3a2a185c09d2
quynIT <trungquyen2902@gmail.com>
2024-09-22 04:34:56 +0700

    Fixed conflict

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,9 +3,19 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Homepage from "../components/HomePage/Homepage";
 import TrendSlide from "../components/TrendSlide/TrendSlide";
+import SignIn from '../pages/auth/Sign_in';
+import SignUp from '../pages/auth/Sign_up';
 export const routes = [
     {
         path: '/',
         page: Homepage
+    },
+    {
+        path: '/sign-in',
+        page: SignIn
+    },
+    {
+        path: '/sign-up',
+        page: SignUp
     }
 ]

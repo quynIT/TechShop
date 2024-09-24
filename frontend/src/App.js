f7f4ebd8d5419287548a3ef11e7b2c932baaa90d
quynIT <trungquyen2902@gmail.com>
2024-09-24 17:37:28 +0700

    #Fixed-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,22 @@ import './App.css';
 import {BrowserRouter,Router, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 import Admin from './Layout/Admin.jsx';
-
+import Layout from './Layout/Layout.jsx';
 
 function App() {
   return (
     <BrowserRouter>
           <Routes>
+          <Route  path="/admin" element={<Admin/>} />
             {routes.map((route) => {
               const Page = route.page
               return (
-                // <Route path="/" element={<Layout/>}>
-                //     <Route  path={route.path} element={<Page/>} />
-                // </Route>
-                <Route path="/" element={<Admin/>}>
+                  <Route path="/" element={<Layout/>}>
+                     <Route  path={route.path} element={<Page/>} />
+                  </Route>
+                /* <Route path="/" element={<Admin/>}>
                    <Route  path={route.path} element={<Page/>} />
-                </Route>
+                  </Route> */
               )
             })}
           </Routes>

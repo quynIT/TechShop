c14d07045343014c68f72220acb2e184e0b3fd70
DRobbie39 <phangiadat123@gmail.com>
2024-12-15 01:53:59 +0700

    Create a user registration API on the backend

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,7 +1,33 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require("../models/UserModel")
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser
         try {
-            resolve({})
+            const checkUser = await User.findOne({
+                email: email
+            })
+            //Kiểm tra email đã tồn tại rồi
+            if(checkUser !== null){
+                resolve({
+                    status: 'OK',
+                    message: 'The email is already'
+                })
+            }
+            const createdUser = await User.create({
+                name, 
+                email, 
+                password, 
+                confirmPassword, 
+                phone
+            })
+            if(createdUser){
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: createdUser
+                })
+            }
         }catch(e) {
             reject(e)
         }

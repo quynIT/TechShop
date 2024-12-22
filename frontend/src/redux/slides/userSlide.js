11c069983df10cf6d0cfe2a949f9cfe08321595c
DRobbie39 <phangiadat123@gmail.com>
2024-12-22 15:37:05 +0700

    Implement the admin page decentralization feature

--- frontend/src/redux/slides/userSlide.js
+++ frontend/src/redux/slides/userSlide.js
@@ -7,7 +7,8 @@ const initialState = {
     address: '',
     avatar: '',
     access_token: '',
-    id: ''
+    id: '',
+    isAdmin: false
 }
 
 export const userSlide = createSlice({
@@ -15,7 +16,7 @@ export const userSlide = createSlice({
     initialState,
     reducers: {
         updateUser: (state, action) => {
-            const { name = '', email = '', access_token = '', phone = '', address = '', avatar = '', _id = '' } = action.payload
+            const { name = '', email = '', access_token = '', phone = '', address = '', avatar = '', _id = '', isAdmin } = action.payload
             state.name = name;
             state.email = email;
             state.phone = phone;
@@ -23,6 +24,7 @@ export const userSlide = createSlice({
             state.avatar = avatar;
             state.id = _id;
             state.access_token = access_token;
+            state.isAdmin = isAdmin;
         },
         resetUser: (state) => {
             state.name = '';
@@ -32,6 +34,7 @@ export const userSlide = createSlice({
             state.avatar = '';
             state.id = '';
             state.access_token = '';
+            state.isAdmin = false;
         },
     },
 })

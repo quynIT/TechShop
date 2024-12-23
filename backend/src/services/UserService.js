a2cb9b3927e4fbc79d7772e19ead07ec40b204e2
quynIT <trungquyen2902@gmail.com>
2024-12-23 15:26:56 +0700

    update-role

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,187 +1,188 @@
-const User = require("../models/UserModel")
+const User = require("../models/UserModel");
 //Dùng để mã hóa mật khẩu
-const bcrypt = require("bcrypt")
-const { genneralAccessToken, genneralRefreshToken } = require("./JwtService")
+const bcrypt = require("bcrypt");
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
-    return new Promise(async (resolve, reject) => {
-        const { name, email, password, confirmPassword, phone } = newUser
-        try {
-            const checkUser = await User.findOne({
-                email: email
-            })
-            //Kiểm tra email đã tồn tại rồi
-            if(checkUser !== null){
-                resolve({
-                    status: 'ERR',
-                    message: 'The email is already'
-                })
-            }
-            //Dùng số 10 kết hợp mật khẩu để mã hóa
-            const hash = bcrypt.hashSync(password, 10)
-            const createdUser = await User.create({
-                name, 
-                email, 
-                password: hash,
-                phone
-            })
-            if(createdUser){
-                resolve({
-                    status: 'OK',
-                    message: 'SUCCESS',
-                    data: createdUser
-                })
-            }
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    const { name, email, password, confirmPassword, phone, role } = newUser;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+      //Kiểm tra email đã tồn tại rồi
+      if (checkUser !== null) {
+        resolve({
+          status: "ERR",
+          message: "The email is already",
+        });
+      }
+      //Dùng số 10 kết hợp mật khẩu để mã hóa
+      const hash = bcrypt.hashSync(password, 10);
+      const createdUser = await User.create({
+        name,
+        email,
+        password: hash,
+        phone,
+        role,
+      });
+      if (createdUser) {
+        resolve({
+          status: "OK",
+          message: "SUCCESS",
+          data: createdUser,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const loginUser = (userLogin) => {
-    return new Promise(async (resolve, reject) => {
-        const { email, password } = userLogin
-        try {
-            const checkUser = await User.findOne({
-                email: email
-            })
-            //Kiểm tra email không tồn tại
-            if(checkUser === null){
-                resolve({
-                    status: 'ERR',
-                    message: 'The user is not defined'
-                })
-            }
-            //So sánh mật khẩu client gửi xuống và mật khẩu trong db
-            const comparePassword = bcrypt.compareSync(password, checkUser.password)
-    
-            if(!comparePassword) {
-                resolve({
-                    status: 'ERR',
-                    message: 'The password or user is incorrect'
-                })
-            }
-            const access_token = await genneralAccessToken({
-                id: checkUser.id,
-                isAdmin: checkUser.isAdmin
-            })
-            
-            //Trả về token mới khi access_token hết hạn
-            const refresh_token = await genneralRefreshToken({
-                id: checkUser.id,
-                isAdmin: checkUser.isAdmin
-            })
-            resolve({
-                status: 'OK',
-                message: 'SUCCESS',
-                access_token,
-                refresh_token
-            })
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    const { email, password } = userLogin;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+      //Kiểm tra email không tồn tại
+      if (checkUser === null) {
+        resolve({
+          status: "ERR",
+          message: "The user is not defined",
+        });
+      }
+      //So sánh mật khẩu client gửi xuống và mật khẩu trong db
+      const comparePassword = bcrypt.compareSync(password, checkUser.password);
+
+      if (!comparePassword) {
+        resolve({
+          status: "ERR",
+          message: "The password or user is incorrect",
+        });
+      }
+      const access_token = await genneralAccessToken({
+        id: checkUser.id,
+        role: checkUser.role,
+      });
+
+      //Trả về token mới khi access_token hết hạn
+      const refresh_token = await genneralRefreshToken({
+        id: checkUser.id,
+        role: checkUser.role,
+      });
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        access_token,
+        refresh_token,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const updateUser = (id, data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkUser = await User.findOne({
-                _id: id
-            })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({
+        _id: id,
+      });
 
-            //Kiểm tra email không tồn tại
-            if(checkUser === null){
-                resolve({
-                    status: 'OK',
-                    message: 'The user is not defined'
-                })
-            }
-            
-            const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
-            resolve({
-                status: 'OK',
-                message: 'SUCCESS',
-                data: updatedUser
-            })
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+      //Kiểm tra email không tồn tại
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "The user is not defined",
+        });
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(id, data, { new: true });
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: updatedUser,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const deleteUser = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkUser = await User.findOne({
-                _id: id
-            })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({
+        _id: id,
+      });
 
-            //Kiểm tra email không tồn tại
-            if(checkUser === null){
-                resolve({
-                    status: 'OK',
-                    message: 'The user is not defined'
-                })
-            }
-            
-            await User.findByIdAndDelete(id)
-            resolve({
-                status: 'OK',
-                message: 'Delete user success'
-            })
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+      //Kiểm tra email không tồn tại
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "The user is not defined",
+        });
+      }
+
+      await User.findByIdAndDelete(id);
+      resolve({
+        status: "OK",
+        message: "Delete user success",
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const getAllUser = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const allUser = await User.find()
-            resolve({
-                status: 'OK',
-                message: 'Success',
-                data: allUser
-            })
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const allUser = await User.find();
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: allUser,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const getDetailsUser = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const user = await User.findOne({
-                _id: id
-            })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({
+        _id: id,
+      });
 
-            //Kiểm tra email không tồn tại
-            if(user === null){
-                resolve({
-                    status: 'OK',
-                    message: 'The user is not defined'
-                })
-            }
-            
-            resolve({
-                status: 'OK',
-                message: 'Success',
-                data: user
-            })
-        }catch(e) {
-            reject(e)
-        }
-    })
-}
+      //Kiểm tra email không tồn tại
+      if (user === null) {
+        resolve({
+          status: "OK",
+          message: "The user is not defined",
+        });
+      }
+
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: user,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 module.exports = {
-    createUser,
-    loginUser,
-    updateUser,
-    deleteUser,
-    getAllUser,
-    getDetailsUser
-}
+  createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getAllUser,
+  getDetailsUser,
+};
--- frontend/src/redux/slides/userSlide.js
+++ frontend/src/redux/slides/userSlide.js
@@ -1,45 +1,54 @@
-import { createSlice, isPending } from '@reduxjs/toolkit'
+import { createSlice, isPending } from "@reduxjs/toolkit";
 
 const initialState = {
-    name: '',
-    email: '',
-    phone: '',
-    address: '',
-    avatar: '',
-    access_token: '',
-    id: '',
-    isAdmin: false
-}
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+  avatar: "",
+  access_token: "",
+  id: "",
+  role: "user",
+};
 
 export const userSlide = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        updateUser: (state, action) => {
-            const { name = '', email = '', access_token = '', phone = '', address = '', avatar = '', _id = '', isAdmin } = action.payload
-            state.name = name;
-            state.email = email;
-            state.phone = phone;
-            state.address = address;
-            state.avatar = avatar;
-            state.id = _id;
-            state.access_token = access_token;
-            state.isAdmin = isAdmin;
-        },
-        resetUser: (state) => {
-            state.name = '';
-            state.email = '';
-            state.phone = '';
-            state.address = '';
-            state.avatar = '';
-            state.id = '';
-            state.access_token = '';
-            state.isAdmin = false;
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    updateUser: (state, action) => {
+      const {
+        name = "",
+        email = "",
+        access_token = "",
+        phone = "",
+        address = "",
+        avatar = "",
+        _id = "",
+        role,
+      } = action.payload;
+      state.name = name;
+      state.email = email;
+      state.phone = phone;
+      state.address = address;
+      state.avatar = avatar;
+      state.id = _id;
+      state.access_token = access_token;
+      state.role = role;
     },
-})
+    resetUser: (state) => {
+      state.name = "";
+      state.email = "";
+      state.phone = "";
+      state.address = "";
+      state.avatar = "";
+      state.id = "";
+      state.access_token = "";
+      state.role = "user";
+    },
+  },
+});
 
 //Action creators are generated for each case reducer function
-export const { updateUser, resetUser } = userSlide.actions
+export const { updateUser, resetUser } = userSlide.actions;
 
-export default userSlide.reducer
+export default userSlide.reducer;

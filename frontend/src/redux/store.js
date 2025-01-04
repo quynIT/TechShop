b10d611037fa6a7b9f68aa198403b90dc3022ca8
quynIT <trungquyen2902@gmail.com>
2025-01-04 22:08:53 +0700

    update-order

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import productReducer from './slides/productSlide'
-import userReducer from './slides/userSlide'
-
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./slides/productSlide";
+import userReducer from "./slides/userSlide";
+import orderReducer from "./slides/orderSlide";
 export const store = configureStore({
-    reducer: {
-        product: productReducer,
-        user: userReducer
-    },
-})
+  reducer: {
+    product: productReducer,
+    user: userReducer,
+    order: orderReducer,
+  },
+});
